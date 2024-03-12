e2bec9ed614d345aa83387c154500232819c2b5c
Dimitrios Chalatsoglou <chalatsoglou-dimitrios@web.de>
2024-03-12 10:04:00 +0100

    update scripts

--- run_helm_deploy.sh
+++ run_helm_deploy.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+
+# This script executes the Ansible playbook but just the 'helm-deploy' role
+
 echo "Deploying via Helm..."
-ansible all -i inventory.ini -m include_role -a name=helm_deploy -e @group_vars/all.yml
-echo "Deployments succeeded!"
+
+ansible all -i ./ansible/inventory.ini -m include_role -a name=ansible/roles/helm_deploy -e @ansible/group_vars/all.yml
+
+if [ $(echo $?) == 0 ]; then
+  echo "Deployments succeeded!"
+else
+  echo "Something went wrong!"
+  exit 1
+fi
+
 exit 0
--- run_playbook.sh
+++ run_playbook.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+
+# This script executes the whole Ansible playbook
+
 echo "Executing the Playbook..."
+
 ansible-playbook -i ./ansible/inventory.ini ./ansible/playbook.yml
-echo "Playbook succeeded!"
+
+if [ $(echo $?) == 0 ]; then
+  echo "Playbook succeeded!"
+else
+  echo "Something went wrong!"
+  exit 1
+fi
+
 exit 0
