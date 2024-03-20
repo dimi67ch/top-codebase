631888af25a65507a4ebe3183814bcfed2c123d8
Dimitrios Chalatsoglou <chalatsoglou-dimitrios@web.de>
2024-03-20 13:25:04 +0100

    update scripts for rbac

---
+++ scripts/create_users.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script creates n users on the Kubernetes cluster
+
+echo "Executing the Playbook..."
+
+ansible-playbook -i ./ansible/inventory.ini ./ansible/roles/k8s_rbac/extras/create_users.yaml
+
+if [ $(echo $?) == 0 ]; then
+  echo "Playbook succeeded!"
+else
+  echo "Something went wrong!"
+  exit 1
+fi
+
+exit 0
